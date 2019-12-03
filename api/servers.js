6f9d04781200f5299f33b19d558cbe388a17353e
ddelfaus <ddelfaus@gmail.com>
2019-12-03 14:06:56 -0500

    guided project end

---
+++ api/servers.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const hubsRouter = require('../hubs/hubs-router.js')
+
+
+
+
+
+const server = express();
+
+
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+server.use("/api/hubs", hubsRouter)
+
+module.exports = server;
